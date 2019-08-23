369df0e0722e67bf26fc51d8d14d021b5ef96a52
Ramesh Singh <rameshsingh.jr@gmail.com>
2019-08-23 18:27:26 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -20,7 +20,7 @@ def apply_coupons(cart, coupons)
       temp = {"#{item} W/COUPON" => {
         :price => coupon_hash[:cost]/coupon_hash[:num],
         :clearance => hash[item][:clearance],
-        :count => 
+        :count => coupon_hash[:count]
         }
       }
       if hash["#{item} W/COUPON"].nil?
