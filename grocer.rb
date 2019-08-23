62a8334e0769ec9ae47977d6096abbb747559972
Ramesh Singh <rameshsingh.jr@gmail.com>
2019-08-23 18:00:07 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -65,10 +65,6 @@ end
 
 items =   [
       {"AVOCADO" => {:price => 3.00, :clearance => true}},
-      {"AVOCADO" => {:price => 3.00, :clearance => true}},
-      {"AVOCADO" => {:price => 3.00, :clearance => true}},
-      {"AVOCADO" => {:price => 3.00, :clearance => true}},
-      {"AVOCADO" => {:price => 3.00, :clearance => true}},
       {"KALE" => {:price => 3.00, :clearance => false}},
       {"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
       {"ALMONDS" => {:price => 9.00, :clearance => false}},
