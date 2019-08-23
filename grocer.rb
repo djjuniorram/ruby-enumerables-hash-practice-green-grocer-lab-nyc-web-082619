357fd244bf8d679665a52eb77399bd9569460cf4
Ramesh Singh <rameshsingh.jr@gmail.com>
2019-08-23 17:47:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -15,18 +15,16 @@
 #end
 
 
-def consolidate_cart(cart)
-  hash = {}
-  cart.each do |item_hash|
-    item_hash.each do |name, price_hash|
-      if hash[name].nil?
-        hash[name] = price_hash.merge({:count => 1})
-      else
-        hash[name][:count] += 1
-      end
-    end
-  end
-  hash
+def consolidate_cart(cart) 
+  new_cart = {} 
+  cart.each do |items_array| 
+    items_array.each do |item, attribute_hash| 
+      new_cart[item] ||= attribute_hash 
+      new_cart[item][:count] ? new_cart[item][:count] += 1 :   
+      new_cart[item][:count] = 1 
+  end 
+end 
+new_cart 
 end
 
 def apply_coupons(cart, coupons)
